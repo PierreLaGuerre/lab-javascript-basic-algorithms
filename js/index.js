11f8372a5e2ed433e118a849a1601f06a7dffba7
Pierangelo Guerrero <pierangelo.guerrero@gmail.com>
2022-10-25 22:32:18 +0200

    done

--- js/index.js
+++ js/index.js
@@ -1,6 +1,33 @@
 // Iteration 1: Names and Input
-//
+
+let hacker1 = 'James'
+
+let hacker2 = 'Bob'
+
+console.log(`The driver name is ${hacker1}`)
+console.log(`The navigator name is ${hacker2}`)
+
 // Iteration 2: Conditionals
 
+if (hacker1.length > hacker2.length) {
+console.log(`The driver has the longest name, it has ${hacker1.length} characters.`)
+}
+else if (hacker2.length > hacker1.length) {
+    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`)
+} else {
+    console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`)
+}
 
 // Iteration 3: Loops
+
+console.log(hacker1.toUpperCase().split("").join(" "))
+
+function reverseString(str) {
+    var newString = "";
+    for (var i = str.length - 1; i >= 0; i--) {
+        newString += str[i];
+    }
+    console.log(newString)
+}
+reverseString(hacker2);
+
